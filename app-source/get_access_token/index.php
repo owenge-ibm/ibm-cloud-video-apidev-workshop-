e4e6d9501b2f0a119ce310c135c4a847cdcb3a6b
owenge-ibm <33232934+owenge-ibm@users.noreply.github.com>
2017-10-31 11:13:57 -0400

    create index.php

---
+++ app-source/get_access_token/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>OAuth Redirect Processor</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php include './getAuthToken.php'; ?>
+</head>
+<body>
+</body>
+</html>
